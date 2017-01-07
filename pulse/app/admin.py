d4b98b0978032487deddb5207812d54a2c8ec648
Randy Mujica Diaz <mujicadiazr@gmail.com>
2017-01-07 01:22:13 -0500

    Export as CSV action - it don't work yet

--- pulse/app/admin.py
+++ pulse/app/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 
 # Register your models here.
 from models import Categoria, Enlace
+from actions import export_as_csv_action
+
+class EnlaceAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'enlace', 'categoria', 'imagen_voto', 'es_popular',)
+    list_filter = ('categoria', 'usuario',)
+    search_fields = ('categoria__titulo','usuario__email',)
+    list_editable = ('titulo', 'enlace', 'categoria',)
+    list_display_links = ('es_popular',)
+    actions = [export_as_csv_action]
+
+    def imagen_voto(self, obj):
+        url = obj.mis_votos_en_imagen_rosada()
+        tag = '<img src="%s">' % url
+        return tag
+
+    imagen_voto.allow_tags = True
+    imagen_voto.admin_order_field = 'votos'
+
 
 admin.site.register(Categoria)
-admin.site.register(Enlace)
+admin.site.register(Enlace, EnlaceAdmin)
