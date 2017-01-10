f26b5bdeff571700becc7cc3ab5a6ea0c11e739a
Randy Mujica Diaz <mujicadiazr@gmail.com>
2017-01-10 17:35:19 -0500

    Middleware created

---
+++ pulse/app/middleware.py
@@ -0,0 +1,27 @@
+from django.shortcuts import redirect
+from random import choice
+
+paises = ['Colombia', 'Peru', 'Cuba', 'Panama', 'Mexico']
+
+def de_donde_vengo(request):
+    return choice(paises)
+
+class PaisMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        pais = de_donde_vengo(request)
+
+        if pais == 'Mexico':
+            return redirect('http://mojorando.la')
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
